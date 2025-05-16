0705c94016d4c738e7e419e6ced145183f042229
0jun <an0yjun@gmail.com>
2025-05-16 14:27:46 +0900

    빌드오류 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,15 @@
 export { default } from 'eslint-config-vuetify/index.ts.mjs'
+// import baseConfig from 'eslint-config-vuetify/index.ts.mjs'
+
+// export default {
+//   ...baseConfig,
+
+//   // baseConfig.extends, parserOptions, plugins 등도 모두 가져옵니다.
+
+//   rules: {
+//     ...baseConfig.rules,
+
+//     // any를 허용하도록 이 규칙을 끕니다.
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+// }
--- vite.config.mts
+++ vite.config.mts
@@ -14,6 +14,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2022',
+  },
   plugins: [
     VueRouter({
       dts: 'src/typed-router.d.ts',
